3e58130089fe4d204c45dc8070f1e8e20d9f72ac
Yue Zhang <34243681+SchenbergZY@users.noreply.github.com>
2022-03-15 09:21:53 +0100

    cancel tf.roll in take_along_axis

--- src/transformers/models/deberta_v2/modeling_tf_deberta_v2.py
+++ src/transformers/models/deberta_v2/modeling_tf_deberta_v2.py
@@ -524,25 +524,26 @@ def pos_dynamic_expand(pos_index, p2c_att, key_layer):
 
 
 def take_along_axis(x, indices, gather_axis):
-    if gather_axis < 0:
-        gather_axis = tf.rank(x) + gather_axis
+    #if gather_axis < 0:
+    #    gather_axis = tf.rank(x) + gather_axis
+    gather_axis = tf.rank(x) + gather_axis
 
-    if gather_axis != tf.rank(x) - 1:
-        pre_roll = tf.rank(x) - 1 - gather_axis
-        permutation = tf.roll(tf.range(tf.rank(x)), pre_roll, axis=0)
-        x = tf.transpose(x, perm=permutation)
-        indices = tf.transpose(indices, perm=permutation)
-    else:
-        pre_roll = 0
+    #if gather_axis != tf.rank(x) - 1:
+    #    pre_roll = tf.rank(x) - 1 - gather_axis
+    #    permutation = tf.roll(tf.range(tf.rank(x)), pre_roll, axis=0)
+    #    x = tf.transpose(x, perm=permutation)
+    #    indices = tf.transpose(indices, perm=permutation)
+    #else:
+    #    pre_roll = 0
 
     flat_x = tf.reshape(x, (-1, tf.shape(x)[-1]))
     flat_indices = tf.reshape(indices, (-1, tf.shape(indices)[-1]))
     gathered = tf.gather(flat_x, flat_indices, batch_dims=1)
     gathered = tf.reshape(gathered, tf.shape(indices))
 
-    if pre_roll != 0:
-        permutation = tf.roll(tf.range(tf.rank(x)), -pre_roll, axis=0)
-        gathered = tf.transpose(gathered, perm=permutation)
+    #if pre_roll != 0:
+    #    permutation = tf.roll(tf.range(tf.rank(x)), -pre_roll, axis=0)
+    #    gathered = tf.transpose(gathered, perm=permutation)
 
     return gathered
 
